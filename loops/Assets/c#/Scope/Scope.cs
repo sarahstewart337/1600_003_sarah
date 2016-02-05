576e414787fd35bec32ed7faa480a7951e72e699
unknown <10732204@CS514-16.ad.uvu.edu>
2016-02-05 09:55:49 -0700

    Scope

---
+++ loops/Assets/c#/Scope/Scope.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scope : MonoBehaviour
+{
+	public int alpha = 5;
+	
+	
+	private int beta = 0;
+	private int gamma = 5;
+	
+	
+	private AnotherClass myOtherClass;
+	
+	
+	void Start ()
+	{
+		alpha = 29;
+		
+		myOtherClass = new AnotherClass();
+		myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+	}
+	
+
+	
+	void Update ()
+	{
+		Debug.Log("Alpha is set to: " + alpha);
+	}
+}
