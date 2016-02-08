7ecf58362d519c630f070e7cb30f45cb8c8653db
unknown <10732204@CS514-16.ad.uvu.edu>
2016-02-08 09:43:18 -0700

    data types

---
+++ loops/Assets/c#/DataTypes/DataType.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataType : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	
+	{
+		//Value type variable
+		Vector3 pos = transform.position;
+		pos = new Vector3(0, 2, 0);
+
+		//reference type variable
+
+		Transform tran = transform;
+		transform.position = new Vector3(0, 4, 0);
+	}
+
+}
