e1b2a2251796db935f222c3d72a006cc4afd81c9
unknown <10732204@CS514-16.ad.uvu.edu>
2016-02-12 09:21:43 -0700

    arrays

--- loops/Assets/c#/Arrays.cs
+++ loops/Assets/c#/Arrays.cs
@@ -10,17 +10,13 @@ public class Arrays : MonoBehaviour
 
 	{
 	
-		players = GameObject.FindGameObjectsWithTag ("Player");
+		players = GameObject.FindGameObjectsWithTag("Player");
 
 		for (int i = 0; i < players.Length; i++)
 		{
-			Debug.Log ("Player Number " + i + " is named " + players [i].name);
+			Debug.Log("Player Number "+i+" is named "+players[i].name);
 		}
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
